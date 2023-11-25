e8b76ac56417952215efe22e34998f7306d630e0
liumstone <am.kushal02@gmail.com>
2023-11-25 23:51:56 +0530

    Add files via upload

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 
 # Create your models here.
+""" 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_verified = models.BooleanField(default=False)
+    email_id = models.CharField(max_length=70)
+
+
+    def __str__(self):
+        return self.user.username """
