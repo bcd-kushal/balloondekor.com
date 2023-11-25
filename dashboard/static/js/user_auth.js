dbae96e47e22cc62d31fe29faed826f3a225936e
liumstone <am.kushal02@gmail.com>
2023-11-25 23:54:27 +0530

    Add files via upload

---
+++ dashboard/static/js/user_auth.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+/* 
+
+fetch("/signin/", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+})
+    .then( (response) => {
+        return response.json();
+    })
+    .then( (data) => {
+        console.log(data);
+    })
+    .catch( (error) => {
+        console.error("Error:", error);
+    }); */
