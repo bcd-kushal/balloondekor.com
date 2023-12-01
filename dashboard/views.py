a9299e27ad8226e24048a7eef57bb44b6a563061
liumstone <am.kushal02@gmail.com>
2023-12-01 23:46:05 +0530

    Add files via upload

--- dashboard/views.py
+++ dashboard/views.py
@@ -16,10 +16,17 @@ from .get_weather import get_weather as weather
 import razorpay
 
 from .models import PaymentRecord as Rec
+from .utils import get_date_time_rn
 
 from django.views.decorators.csrf import csrf_exempt
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from datetime import datetime
+
+ATLAS_URI = "mongodb+srv://kushalkumar:iamkushal02@firstcluster.4mlqtzs.mongodb.net/?retryWrites=true&w=majority"
+        
+
 
 # from .forms import SignUpForm
 
@@ -336,11 +343,186 @@ def payment_success(req):
 
 
 ########[ DATABASES ]#######################################################
-""" ===== CREATE DATABASE ===================== """
+""" ===== SELECT DATABASE PAGE ===================== """
 def create_database(req):
+    global ATLAS_URI
+
+    if req.method == "POST":
+
+        if "SQLbox" in req.POST:
+
+            mongo_client = MongoClient(ATLAS_URI,server_api = ServerApi('1'))
+
+            atlas_db = mongo_client.get_database("users-crud-db")
+            atlas_admin = atlas_db.admin
+            
+            DB_USER = req.user.username
+
+            OBJ = atlas_admin.find_one({ "user": DB_USER })
+            OBJ = OBJ["database_names"] if OBJ is not None else []
+            OBJ_STR = ','.join(map(str, OBJ))
+
+            print("```````````````````````````",OBJ_STR)
+
+            return render(req,"database_model/createDB/createDB.html",{
+                "loggedin_username": req.user.username or None,
+                "username": req.user.username,
+                "user_desc": "Full Stack Dev",
+                "theme_col": "197, 172, 244",
+                "user_icon": req.user.username[0:1],
+                "status": "green",
+                "databases": OBJ_STR,
+                "makeSQL": 1
+            })
+        
+
+
+
+        if "NoSQLbox" in req.POST:
+
+
+
+            return render(req,"database_model/createDB/createDB.html",{
+                "loggedin_username": req.user.username or None,
+                "username": req.user.username,
+                "user_desc": "Full Stack Dev",
+                "theme_col": "197, 172, 244",
+                "user_icon": req.user.username[0:1],
+                "status": "green",
+                "makeNoSQL": 1
+            })
+    
+
 
     if req.method == "GET":
         return render(req,"database_model/createDB/createDB.html",{
-            "loggedin_username": req.user.username or None
+            "loggedin_username": req.user.username or None,
+            "username": req.user.username,
+            "user_desc": "Full Stack Dev",
+            "theme_col": "197, 172, 244",
+            "user_icon": req.user.username[0:1],
+            "status": "green",
+            "selectDB": 1
         })
     
+
+
+
+
+
+""" ===== CREATE SQL DATABASE ===================== """
+def create_SQL_DB(req):
+    global ATLAS_URI
+
+    if req.method == "POST":
+        db = {
+            "name": req.POST.get("db_name"),
+            "attributes": req.POST.get("db_schema").split('\r\n'),
+            "primary_key": req.POST.get("db_pk"),
+            "foreign_key": req.POST.get("db_fk")
+        }
+
+        
+        try:
+            # mongo_client.admin.command('ping')
+            mongo_client = MongoClient(ATLAS_URI,server_api = ServerApi('1'))
+
+            atlas_db = mongo_client.get_database("users-crud-db")
+            atlas_admin = atlas_db.admin
+            atlas_sql = atlas_db.sql_table
+            # atlas_nosql = atlas_db.nosql_table
+
+
+            TIME_RN = get_date_time_rn()
+
+            ADMIN_DOC_STRUCTURE = {
+                "user": "",
+                "database_names": []
+            }
+
+            SQL_DOC_STRUCTURE = {
+                "user": "",
+                "db_name": "",
+                "primary_key": "",
+                "date_created": TIME_RN,
+                "date_modified": TIME_RN,
+                "attributes": [],
+                "rows": 0,
+                "cols": 0,
+                "description": "",
+                "data": {}
+            }
+
+
+            DB_USER = req.user.username
+            # create SQL table under this username
+
+            if(atlas_sql.find_one({ "user": DB_USER, "db_name": db["name"] }) == None):
+                SQL_DOC_STRUCTURE["user"] = DB_USER
+                SQL_DOC_STRUCTURE["db_name"] = db["name"]
+                SQL_DOC_STRUCTURE["attributes"] = db["attributes"]
+                SQL_DOC_STRUCTURE["primary_key"] = db["primary_key"]
+                SQL_DOC_STRUCTURE["cols"] = len(db["attributes"])
+
+                atlas_sql.insert_one(SQL_DOC_STRUCTURE)
+
+                # print(f"########### successfully added {db['name']} to user:{DB_USER} ###########################")
+
+
+
+            OBJ = atlas_admin.find_one({ "user": DB_USER })
+
+            if(OBJ == None):
+                ADMIN_DOC_STRUCTURE["user"] = DB_USER
+                ADMIN_DOC_STRUCTURE["database_names"].append(db["name"])
+                atlas_admin.insert_one(ADMIN_DOC_STRUCTURE)
+            
+            else: 
+                OBJ["database_names"].append(db["name"])
+                atlas_admin.update_one({ "user": DB_USER }, { '$set': { "database_names" : OBJ["database_names"] }}) 
+
+
+
+
+        except Exception as e:
+            print(f"\n\n  [  ERR: {e}  ] \n\n")
+
+
+
+
+        return redirect('/')
+
+
+
+
+
+
+
+""" 
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { ref, child, get, set } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey:                 "AIzaSyBO2LjF2Vau4wAhjiSB6i-xpUfIrWMv67w",
+    authDomain:             "newsletter-5be1e.firebaseapp.com",
+    databaseURL:            "https://newsletter-5be1e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId:              "newsletter-5be1e",
+    storageBucket:          "newsletter-5be1e.appspot.com",
+    messagingSenderId:      "422358715800",
+    appId:                  "1:422358715800:web:58f50be1736602d1be6fed"
+};
+
+const app = initializeApp(firebaseConfig);
+const dataBase = getDatabase(app);
+
+
+
+ """
+
+
+
+
+
+
+
